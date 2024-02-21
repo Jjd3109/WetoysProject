40c392e5963ca1b611fbaea3cbfb6c7b718c0a0d
정종두 <jongdoo2001@naver.com>
2024-02-21 15:26:00 +0900

    Initial commit

---
+++ src/main/java/com/wetoys/wetoysproject/WetoysProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wetoys.wetoysproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetoysProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetoysProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wetoys/wetoysproject/WetoysProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wetoys.wetoysproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetoysProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
