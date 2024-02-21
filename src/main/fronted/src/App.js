9846726c70c4a24a3076aac48483b348133cd074
정종두 <jongdoo2001@naver.com>
2024-02-21 18:31:25 +0900

    settigs

--- src/main/fronted/src/App.js
+++ src/main/fronted/src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Header from "./Header";
+import Example from "./Example";
 
 function App() {
+  const [hello, setHello] = useState('');
+
+  useEffect(() => {
+    axios.get('/api/test')
+        .then((res) => {
+          //setHello(res.data);
+          console.log();
+        })
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+      <div className="App">
+          <Header />
+          <Example />
+
+          {
+
+          }
+
+        백엔드 데이터 : {hello}
+          <div className="">
+              <h2 className="text-blue-500 text-xl font-bold">Hello, React!</h2>
+              <p className="text-lg font-medium">Hello, Typescript!</p>
+          </div>
+      </div>
+
   );
 }
 
---
+++ src/main/fronted/src/Header.js
@@ -0,0 +1,12 @@
+import {Fragment, useEffect, useState} from "react";
+import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
+
+function Header(){
+    return (
+        <div className="bg-white">
+
+        </div>
+    );
+}
+
+export default Header;
