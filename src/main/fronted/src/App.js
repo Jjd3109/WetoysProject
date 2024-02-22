6b5b5aa37ba6a33991921189e574b64a3be7aa10
정종두 <jongdoo2001@naver.com>
2024-02-22 18:21:29 +0900

    Feat : 회원 가입 기능 및 DB 연결 및 Toast.ui 사용

--- src/main/fronted/src/App.js
+++ src/main/fronted/src/App.js
@@ -2,37 +2,52 @@ import logo from './logo.svg';
 import {useEffect, useState} from "react";
 import axios from "axios";
 import Header from "./Header";
-import Example from "./Example";
+import Example from "./view/Example";
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import ItemList from "./view/ItemList";
+import Detail from "./view/Detail";
+import MemberJoin from "./view/MemberJoin";
+import ItemCreate from "./view/ItemCreate";
+
+
 
 function App() {
   const [hello, setHello] = useState('');
 
   useEffect(() => {
-    axios.get('/api/test')
-        .then((res) => {
-          //setHello(res.data);
-          console.log();
-        })
+    // axios.get('/api/test')
+    //     .then((res) => {
+    //       //setHello(res.data);
+    //         const value = res.data;
+    //         console.log(res.data);
+    //         console.log(value['이름']);
+    //         setHello(res.data);
+    //
+    //     })
   }, []);
 
 
   return (
-
-
       <div className="App">
           <Header />
           <Example />
+          {/*백엔드 데이터 : {hello['이름']}*/}
+          {/*<div className="">*/}
+          {/*    <h2 className="text-blue-500 text-xl font-bold">Hello, React!</h2>*/}
+          {/*    <p className="text-lg font-medium">Hello, Typescript!</p>*/}
+          {/*</div>*/}
 
-          {
+        <Routes>
+            <Route path="/itemList" element={<ItemList />}></Route>
+            <Route path="/Detail/1" element={<Detail />}></Route>
+            <Route path="/MemberJoin" element={<MemberJoin />}></Route>
+            <Route path="/ItemCreate" element={<ItemCreate />}></Route>
+        </Routes>
 
-          }
 
-        백엔드 데이터 : {hello}
-          <div className="">
-              <h2 className="text-blue-500 text-xl font-bold">Hello, React!</h2>
-              <p className="text-lg font-medium">Hello, Typescript!</p>
-          </div>
       </div>
+
+
 
   );
 }
--- src/main/java/com/wetoys/wetoysproject/WetoysProjectApplication.java
+++ src/main/java/com/wetoys/wetoysproject/WetoysProjectApplication.java
@@ -2,8 +2,10 @@ package com.wetoys.wetoysproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WetoysProjectApplication {
 
 	public static void main(String[] args) {
